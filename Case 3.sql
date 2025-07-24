db83418b781426c70c4308e2318015b767d4e0ec
Gideon Oluniyi <95762770+Giddyyy@users.noreply.github.com>
2025-07-24 21:03:30 +0100

    Update Case 3.sql

--- Case 3.sql
+++ Case 3.sql
@@ -1,6 +1,6 @@
 -- Using savings_savingsaccount and plans_plan tables -- Join
 -- Account type (Savings or Investment) column
--- using CASE to set type conditions
+-- using CASE to set type conditions:
 select s.plan_id, s.owner_id,
 	case
 		when p.is_regular_savings = 1 then 'Savings'
